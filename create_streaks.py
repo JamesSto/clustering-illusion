5ccd476cd95415ac8f159817ef68a04a711d43cb
James Stoyell <jms852@cornell.edu>
2017-12-10 22:06:09 -0500

    final script

--- create_streaks.py
+++ create_streaks.py
@@ -1,31 +1,49 @@
-from random import random, choice
+from __future__ import division
+from random import random, randint, uniform
 from termcolor import colored
+from tqdm import tqdm
+
+HIT = "H"
+MISS = "M"
+
+class Streak(object):
+    def __init__(self, avg, sequence):
+        self.avg = avg
+        self.sequence = sequence
+
+        total_hits = 0
+        streaks = 0
+        for p, n in zip(self.sequence, self.sequence[1:]):
+            if p == HIT:
+                total_hits += 1
+                if n == HIT:
+                    streaks += 1
+
+        self.streakiness = streaks/total_hits if total_hits else None
+
+    def is_streaky(self):
+        return self.streakiness > self.avg
+
+    def __str__(self):
+        avg = str(round(self.avg,2))
+        streakiness = str(round(self.streakiness,2))
+        sequence = " ".join(self.sequence)
+        return avg + "," + streakiness + "," + str(self.is_streaky()) + "," + sequence
+
+def create_streak(length, avg):
+    sequence = []
+    for _ in xrange(length):
+        if random() < avg:
+            sequence.append(HIT)
+        else:
+            sequence.append(MISS)
+    
+    return Streak(avg, sequence)
 
 
-HIT = colored("H", 'green')
-MISS = colored("M", 'red')
-
-def create_streak(length, bias):
-	streak = []
-	streak.append(choice([HIT, MISS]))
-	for _ in xrange(length-1):
-		prev = streak[-1]
-		if random() < bias:
-			streak.append(prev)
-		else:
-			streak.append(MISS if prev == HIT else HIT)
-	return streak
-
-
-rec = []
-for _ in xrange(10):
-	if random() < .5:
-		print " ".join(create_streak(10, 0.7))
-		rec.append("streaky")
-	else:
-		print " ".join(create_streak(10, .5))
-		rec.append("normal")
-	print ""
-
-for x in rec:
-	print x
+s = []
+print "avg,streakiness,is_streaky,sequence"
+for _ in xrange(500):
+    x = create_streak(50, uniform(0.3, 0.9))
+    print x
+    s.append(x)
